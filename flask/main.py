84f7603731ea971f964a397a2975b8cdd6d02b84
Peggy Li <peggy.li@duke.edu>
2014-03-30 02:40:04 -0400

    got basic text/call working

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, jsonify, session, render_template
+from flask import Flask, request, jsonify, session, render_template, Response, url_for
 
-from twilio_wrapper import sms_message
+from twilio_wrapper import sms_message, phone_call
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+PHONE_NUMBERS = [
+	'+13013570811',
+	'+14085156785'
+]
 
 @app.route('/sendtest', methods=['GET', 'POST'])
 def send_test():
@@ -13,14 +18,41 @@ def send_test():
         return name if name else "Error"
 
 
+@app.route('/add-user', methods=['GET', 'POST'])
+def add_user():
+	return ''
+
+
+@app.route('/text', methods=['GET', 'POST'])
+def send_sms():
+	if request.method == 'POST':
+		message = request.json['message']
+		for num in PHONE_NUMBERS:
+			sms_message.send(message, num)
+		return 'Success'
+
+@app.route('/call', methods=['GET', 'POST'])
+def call_number():
+	if request.method == 'POST':
+		message = request.json['message']
+		with app.test_request_context():
+			url = url_for('xml', msg=message, _external=True)
+			print url
+		for num in PHONE_NUMBERS:
+			phone_call.call(url, num)
+
+@app.route('/gen-xml', methods=['GET'])
+def xml():
+	message = request.args.get('msg')
+	return Response(
+	"""<response>
+		<say>%s</say>
+	</response>""" % message, mimetype='text/xml')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
-
-@app.route('/xmldump', methods=['GET', 'POST'])    
-# @app.route('/xmldump/<xfile>')
-def xml():
-	return "hello"
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", debug=True)
--- flask/twilio_wrapper/phone_call.py
+++ flask/twilio_wrapper/phone_call.py
@@ -2,33 +2,16 @@ from twilio.rest import TwilioRestClient
 
 from twilio_settings import *
 
-import hashlib, os
+import hashlib, urllib2
 
-XML_DIRECTORY = 'xmldump'
+ROOT = ''
 
-def call(message, to_number, from_number = SENDER):
+def call(url, to_number, from_number = SENDER):
 	"""
-	Make a call with a voice reading the message (string)
+	Make a call with a voice reading the message at url
 	"""
-	fname = create_say_xml(message)
-	print to_number, SENDER, fname
-	print client.calls
-	call = client.calls.create(to_number, SENDER, fname)
-
-	print call.sid
-
-def create_say_xml(message):
-	fname = "https://www.dropbox.com/s/3nsxv9frx3cih31/63325070d56c3580500f5f64d0b1e27bb3b68961f166959fe51369a1.xml"
-	# with open(fname, 'w') as f:
-	# 	f.write('<Response>')
-	# 	f.write('\t<Say>%s</Say>' % message)
-	# 	f.write('</Response>')
-	return fname
-
-
-def get_hash(message):
-	return hashlib.sha224(message).hexdigest()
-
+	
+	call = client.calls.create(to_number, from_number, url)
 
 if __name__ == '__main__':
 	call("Hello Peggy", "+13013570811")
